6c036d56083110b391f041e1fbf6a4035738079c
= <=>
2022-09-27 10:06:35 +0500

    works

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,7 +1,18 @@
+import { useState, useEffect } from "react";
 import { PeopleAltTwoTone } from "@mui/icons-material";
 import { Button, Link } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 function Dashboard() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      navigate("/login");
+    }
+  }, []);
+
   return (
     <div className="flex flex-col justify-center items-center">
       <header className="flex w-screen justify-between items-center gap-5 px-10">
